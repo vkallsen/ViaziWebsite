2f200ce95823360161a5deb2e3a9864dc54a27aa
vkallsen <vkallsen@gmail.com>
2018-08-13 22:17:37 -0400

    converted html files to php

--- index.php
+++ index.php
@@ -21,29 +21,8 @@
   <!-- https://www.w3schools.com/php/php_includes.asp 
   http://afterlecture.com/forums/topic/how-to-copy-header-footer-in-every-html-page/  -->
   <!-- Header -->
-	<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #5A3583">
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	  </button>
-
-	  <div class="collapse navbar-collapse" id="topnavbar">
-		<ul class="navbar-nav ml-auto">
-		  <li class="nav-item active">
-			<a class="nav-link" href="index.html">Home</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="approach.html">Approach</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="about.html">About</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="contact.html">Contact</a>
-		  </li>
-		</ul>
-	  </div>
-	</nav>
-	 <!-- End Header -->
+	<?php include ("header.php"); ?>
+  <!-- End Header -->
 	<div id="content">
 		<div class="jumbotron jumbotron-fluid" style="background: linear-gradient(#e87fba, black)">
 			<div class="container container-fluid">
@@ -81,33 +60,12 @@
 			</div>	
 		</div>
 	</div>
-	 <!-- Footer -->
+
 	 <!-- https://stackoverflow.com/questions/2189452/when-to-use-margin-vs-padding-in-css 
 	 https://getbootstrap.com/docs/4.0/utilities/spacing/
 	 -->
-		<footer class="my-5 pt-5">
-		<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #5A3583">
-	<!-- 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		  </button> -->
-		  <div class="collapse navbar-collapse" id="bottomnavbar">
-			<div class="navbar-nav mr-auto">
-			<span class="navbar-brand">
-					Viazi
-			</span>
-			<span class="navbar-text">
-					Pittsburgh, PA 15213<br>(508) 494-5843<br>isaac@viazi.co
-			</span>
-			</div>
-			<div class="navbar-nav ml-auto">
-				<span class="navbar-text">
-					Website (c) Viazi LLC <br>
-					<a href=https://www.victoriakallsen.com>Website designed by Victoria Kallsen</a><br>
-					<a href="image.html">Image Credits</a>
-			</div>
-		  </div>
-		</nav>
-	</footer>
+	<!-- Footer -->
+	<?php include ("footer.php"); ?>
 	<!-- End Footer -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
