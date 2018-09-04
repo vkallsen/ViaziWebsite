00d594b0a44416a633c49aab97d1c654d79266a8
vkallsen <vkallsen@gmail.com>
2018-09-04 15:02:03 -0400

    fixed typo

--- deploy_header.php
+++ deploy_header.php
@@ -1,5 +1,5 @@
 <!-- Header -->
-	<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #5A3583"> -->
+	<nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #5A3583">
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	  </button>
