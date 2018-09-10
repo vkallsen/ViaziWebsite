98f6abe761f3fffda7e48cf23f69d70407e3aea0
vkallsen <vkallsen@gmail.com>
2018-09-10 16:50:36 -0400

    Correct file names & version that now works with PHP upload to AWS.

---
+++ about.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/style.css">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/timeline.css">
+
+    <title>Viazi - About</title>
+  </head>
+  <body>
+  <!-- Header -->
+	<?php include ("header.php"); ?>
+  <!-- End Header -->
+	<div class="jumbotron jumbotron-fluid" style="background: linear-gradient(#e87fba, black)">
+		<div class="container container-fluid">
+			<div class="row">
+					<img src="img/penguin.png" class="img-responsive" width="75" height="93">
+					<h1 class="display-4" style="color:white">Viazi</h1>
+			</div>
+		</div>
+	</div>
+  <!-- https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form 
+  https://www.w3schools.com/bootstrap/bootstrap_forms.asp
+  -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+				<h2 class="page-header">Our Story</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+				<p><br>Viazi LLC was created by Isaac Rudich and Avi Rudich in 2017. Isaac graduated from Carnegie Mellon University in 2017 with two degrees: Production Technology & Management (College of Fine Arts) and Finance (Tepper School of Business). Avi expects to graduate from Carnegie Mellon University in 2020 with a degree in Electrical Engineering from the College of Engineering</p>			
+			</p>
+			</div>
+		</div>
+		<div class="row">
+			<p><br></p>
+		</div>
+		<div class="row">	
+			<div class="col-lg-8 col-lg-offset-2"> 
+				<h2 class="page-header">Timeline</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2"> 
+					<ul class="timeline">
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-off"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">March 2017</h4>
+								</div>
+								<div class="timeline-body">
+									<p>Isaac’s undergraduate advisor tells him that he spends most of his time working on next semester’s schedule. The advisor has looked for software to solve this problem, but none can handle the complex scenarios that arise when space and time are both heavily constrained.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">April 2017</h4>
+								</div>
+								<div class="timeline-body">
+									<p>Isaac and Avi begin work on an algorithm and platform to solve the scheduling problem.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">September 2017</h4>
+								</div>
+								<div class="timeline-body">
+									<p>Isaac and Avi have created an algorithm that can solve the problem in hours, instead of the weeks that other modern algorithms take. Viazi LLC is officially incorporated.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">December 2017</h4>
+								</div>
+								<div class="timeline-body">
+									<p>Viazi demonstrates an installable software that takes in necessary constraints and creates a schedule. Our first client expresses immense interest in using the software, but requests a website be made for long term use.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">July 2018</h4>
+								</div>
+								<div class="timeline-body">
+									<p>A breakthrough for our algorithm reduces its run time to less than a minute on most data sets.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">August 2018</h4>
+								</div>
+								<div class="timeline-body">
+									<p>The website version of Viazi solver goes live.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<!-- <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 class="timeline-title">August 2018</h4>
+								</div>
+								<div class="timeline-body">
+									<p>The website version of Viazi solver goes live.</p>
+								</div>
+							</div> -->
+						</li>
+					</ul>
+			</div>
+		</div>
+    </div>
+	<!-- Footer -->
+	<?php include ("footer.php"); ?>
+	<!-- End Footer -->
+      <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/style.css">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/timeline.css">
+
+    <title>Viazi - Home</title>
+  </head>
+  <body>
+  <!-- https://www.w3schools.com/php/php_includes.asp 
+  http://afterlecture.com/forums/topic/how-to-copy-header-footer-in-every-html-page/  -->
+  <!-- Header -->
+	<?php include ("header.php"); ?>
+  <!-- End Header -->
+	<div id="content">
+		<div class="jumbotron jumbotron-fluid" style="background: linear-gradient(#e87fba, black)">
+			<div class="container container-fluid">
+				<div class="row">
+						<img src="img/penguin.png" class="img-responsive" width="75" height="93">
+						<h1 class="display-4" style="color:white">Viazi</h1>
+				</div>
+				<div class="row">
+					<p class="lead" style="color:white">Course scheduling made easy.</p>
+					<br>
+					<br>
+					<br>
+				</div>
+			</div>
+		</div>
+		<div class="container marketing" style="text-align:center">
+			<div class="row">
+				<div class="col-lg-6">
+					<img src="img/calendar.png" class="img-responsive" width="216" height="216">	
+					<h2>
+						<a class="btn btn-secondary" href="howitworks.php" role="button">What We Do</a>
+					</h2>				
+					<p>
+						<!-- Deploying custom applications to solve our customers' roadblocks. -->
+						The only difference between “This is too complicated for software” and “How soon can I use this?” is a ten-minute demo.
+					</p>
+				</div>
+				<div class="col-lg-6">
+					<img src="img/users.svg" class="img-responsive" width="216" height="216">
+					<br>
+					<h2><a class="btn btn-secondary" href="contact.php" role="button">Contact Us</a></h2>
+					<p>
+						If you are interested in trying out the software or have questions about how it works, we would love to hear from you. 
+					</p>
+				</div>
+			</div>	
+		</div>
+	</div>
+
+	 <!-- https://stackoverflow.com/questions/2189452/when-to-use-margin-vs-padding-in-css 
+	 https://getbootstrap.com/docs/4.0/utilities/spacing/
+	 -->
+	<!-- Footer -->
+	<?php include ("footer.php"); ?>
+	<!-- End Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
