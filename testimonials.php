f720b727002a82e3165a1e9cf2d76bdd575c3640
vkallsen <vkallsen@gmail.com>
2018-09-23 11:11:50 -0400

    added testimonials page

---
+++ testimonials.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/style.css">
+	<link rel="stylesheet" type="text/css" href="https://s3.us-east-2.amazonaws.com/viazi-website/timeline.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"> 
+
+    <title>Viazi - Testimonials</title>
+  </head>
+  <body>
+    <!-- Header -->
+	<?php include ("header.php"); ?>
+  <!-- End Header -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="page-header">Testimonials</h2>
+			</div>
+		</div>
+		<div class="row my-2">
+				<div class="card w-100 p-2">
+					<div class="card-body d-flex flex-column">
+						<blockquote class="blockquote mb-0">
+							<p>The schedule solver is miraculous. The idea that I could change one or two things and have it resolve the whole puzzle is an enormous relief. Thanks for creating this.</p>
+						</blockquote>
+					</div>
+					<div class="mt-auto p-2">
+							<h4 class="card-title">Melissa Cicozi</h4>
+							<h6 class="card-subtitle mb-2 text-muted">Assistant Head of the School of Design at Carnegie Mellon University</h6>
+					</div>
+				</div>
+		</div>
+		<div class="row my-2">
+				<div class="card w-100 p-2">
+					<div class="card-body d-flex flex-column">
+						<blockquote class="blockquote mb-0">
+							<p>Text</p>
+						</blockquote>
+					</div>
+					<div class="mt-auto p-2">
+							<h4 class="card-title">Name</h4>
+							<h6 class="card-subtitle mb-2 text-muted">Position</h6>
+					</div>
+				</div>
+		</div>
+	</div> 
+	<!-- Footer -->
+	<?php include ("footer.php"); ?>
+	<!-- End Footer -->
+      <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
